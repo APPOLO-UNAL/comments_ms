c724cc77e0a8527809e2108979625317b18cd2c0
Juan Zambrano <juzambranol@unal.edu.co>
2024-03-09 18:43:01 -0500

    feat: routing created

---
+++ src/routes/comments.routes.ts
@@ -0,0 +1,40 @@
+import express from 'express'
+
+// Initializations
+
+const router = express.Router()
+
+import  {
+  getCommentHandler,
+  getCommentByItemIdHandler,
+  postCommentHandler,
+  replyCommentHandler,
+  deleteCommentHandler,
+  editCommentHandler
+} from '../controllers/comments.controller'
+
+// Endpoint routes
+
+// Get all method (get all)
+
+router.get('/comments', getCommentHandler)
+
+//Get all Item comments 
+
+router.get('/:idItem/comments',getCommentByItemIdHandler)
+// Create  comment 
+router.post('/comments/', postCommentHandler)
+
+// Edit Comment/Reply
+
+router.put(`/comments/:idComment`,editCommentHandler) 
+
+// Delete comment/Reply (and his sons)
+router.delete('/comments/:idComment',deleteCommentHandler)
+// Create reply 
+router.post('/:idItem/comments/:idComment/', replyCommentHandler)
+
+
+
+// Exportation
+module.exports = router
