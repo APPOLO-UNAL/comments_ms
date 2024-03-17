93b0ffe607ae63ab1872776e3b4d952775e9c49d
Juan Zambrano <juzambranol@unal.edu.co>
2024-03-17 13:38:40 -0500

    fix: env variables handle

--- src/database.ts
+++ src/database.ts
@@ -3,10 +3,12 @@ import mongoose from 'mongoose';
 
 
 dotenv.config()
+const CONNECTIONSTRINGTEMPLATE:String=process.env.CONNECTIONSTRINGTEMPLATE as string
+const CONNECTIONSTRING=CONNECTIONSTRINGTEMPLATE
+    .replace("<user>",process.env.MONGOUSER as string)
+    .replace("<password>",process.env.MONGOPASSWORD as string)
 
-
-
-mongoose.connect(process.env.MONGOURI as string, {})
+mongoose.connect(CONNECTIONSTRING, {})
 .then(()=>console.log("It's connected!"))
 .catch(err=>console.log(`The connection has failed :\n${err}`));
 
