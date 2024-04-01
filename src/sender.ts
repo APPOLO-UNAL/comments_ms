e25921a4e7c45f53b92aaa665c211b4c52e3ad3a
Lanna-27 <lalacast2000@gmail.com>
2024-03-31 20:49:54 -0500

    sender update

--- src/sender.ts
+++ src/sender.ts
@@ -1,15 +1,31 @@
 import * as amqp from 'amqplib';
 
-const queueName = 'hello';
-const message = 'Hello RabbitMQ from TypeScript!';
+const queueName = 'notifications';
+//const message = 'Hello RabbitMQ from TypeScript!';
 
-async function send() {
-    const connection = await amqp.connect('amqp://localhost');
-    const channel = await connection.createChannel();
+interface Message {
+    title: string;
+    message: string;
+    id_user: string;
+  }
 
+async function send(title: string, body: string, id_user:string) {
+
+    const connection = await amqp.connect('amqp://guest:guest@localhost:5672/');
+    const channel = await connection.createChannel()
     await channel.assertQueue(queueName, { durable: false });
 
-    channel.sendToQueue(queueName, Buffer.from(message));
+    // Create a message
+    let message: Message = {
+        title: title,
+        message: body,
+        id_user: id_user
+      };
+    
+    // Convert the message to a JSON string
+    const jsonBody: string = JSON.stringify(message);
+
+    channel.sendToQueue(queueName, Buffer.from(jsonBody));
     console.log(" [x] Sent '%s'", message);
 
     setTimeout(() => {
@@ -18,7 +34,7 @@ async function send() {
     }, 500);
 }
 
-send().catch((err) => {
-    console.error("Error:", err);
-    process.exit(1);
-});
+// send("Title", "Body", "1").catch((err) => {
+//     console.error("Error:", err);
+//     process.exit(1);
+// });
