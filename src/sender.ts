71fdff3e0f63ea447e30639ccb520434bff3e508
MateoG404 <mgutierrezca@unal.edu.co>
2024-04-01 20:14:29 -0500

    refactor: Updated rabbit mq to send notifications

--- src/sender.ts
+++ src/sender.ts
@@ -1,18 +1,16 @@
 import * as amqp from 'amqplib';
 
 const queueName = 'notifications';
-//const message = 'Hello RabbitMQ from TypeScript!';
 
 interface Message {
     title: string;
     message: string;
     id_user: string;
-  }
+}
 
 export async function send(title: string, body: string, id_user:string) {
-
     const connection = await amqp.connect('amqp://guest:guest@my-rabbit:5672/');
-    const channel = await connection.createChannel()
+    const channel = await connection.createConfirmChannel();
     await channel.assertQueue(queueName, { durable: false });
 
     // Create a message
@@ -20,21 +18,18 @@ export async function send(title: string, body: string, id_user:string) {
         title: title,
         message: body,
         id_user: id_user
-      };
+    };
     
     // Convert the message to a JSON string
     const jsonBody: string = JSON.stringify(message);
 
-    channel.sendToQueue(queueName, Buffer.from(jsonBody));
-    console.log(" [x] Sent '%s'", message);
+    // Send the message and wait for confirmation
+    channel.sendToQueue(queueName, Buffer.from(jsonBody), {}, (err, ok) => {
+        if (err) {
+            console.error("Error:", err);
+        } else {
+            console.log(" [x] Sent '%s'", message);
+        }
 
-    setTimeout(() => {
-        connection.close();
-        process.exit(0);
-    }, 500);
+    });
 }
-
-// send("Title", "Body", "1").catch((err) => {
-//     console.error("Error:", err);
-//     process.exit(1);
-// });
