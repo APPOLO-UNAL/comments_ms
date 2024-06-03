95f7d6e7290cce906f775db79241bc5efba2bddf
Juan Zambrano <juzambranol@unal.edu.co>
2024-06-03 17:28:02 -0500

    env deploy fixed

--- src/database.ts
+++ src/database.ts
@@ -3,12 +3,9 @@ import mongoose from 'mongoose';
 
 
 dotenv.config()
-const CONNECTIONSTRINGTEMPLATE:String=process.env.CONNECTIONSTRINGTEMPLATE as string
-const CONNECTIONSTRING=CONNECTIONSTRINGTEMPLATE
-    .replace("<user>",process.env.MONGOUSER as string)
-    .replace("<password>",process.env.MONGOPASSWORD as string)
+const CONNECTIONSTRING="mongodb://juzambranol:mongodb1228@ac-8eq3gxv-shard-00-00.ruvhdcp.mongodb.net:27017,ac-8eq3gxv-shard-00-01.ruvhdcp.mongodb.net:27017,ac-8eq3gxv-shard-00-02.ruvhdcp.mongodb.net:27017/comments_db?ssl=true&replicaSet=atlas-hp2bbp-shard-0&authSource=admin&retryWrites=true&w=majority&appName=DBCluster"
 
 mongoose.connect(CONNECTIONSTRING, {})
-.then(()=>console.log("It's connected!"))
+.then(()=>console.log("It's connected :D !"))
 .catch(err=>console.log(`The connection has failed :\n${err}`));
 
